c61b5c4af1f500a98cdf650e5618d7b00516238c
Corey Abshire <corey.abshire@gmail.com>
2023-10-22 21:09:29 -0500

    Add a basic test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders specials', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = screen.getByText(/This weeks' specials!/i);
   expect(linkElement).toBeInTheDocument();
 });
